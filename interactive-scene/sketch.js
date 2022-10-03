ba95fc0056f80041ef0138b156b7b13f49eec096
bucky6 <114016486+bucky6@users.noreply.github.com>
2022-10-03 11:57:57 -0600

    made interactive scene project

---
+++ interactive-scene/sketch.js
@@ -0,0 +1,15 @@
+// Music Simulator
+// Bucky Pederson-Bradbury
+// October 11, 2022
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
