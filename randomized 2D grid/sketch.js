20a3ffa01803a6e2c15d8a32e84ad3aa55a77b7b
bucky6 <114016486+bucky6@users.noreply.github.com>
2022-10-27 12:12:38 -0600

    game of life

---
+++ randomized 2D grid/sketch.js
@@ -0,0 +1,62 @@
+// Randomized 2D Grid
+// Bucky Pederson-Bradbury
+// October 26, 2022
+
+let rows = 10;
+let cols = 10;
+let grid;
+let cellWidth;
+let cellHeight;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cellWidth = width/cols;
+  cellHeight = height/rows;
+  grid = createRandom2DArray(cols, rows);
+}
+
+function draw() {
+  background(220);
+  displayGrid(grid);
+}
+
+function displayGrid(grid) {
+  for (let y=0; y<rows; y++) {
+    for (let x=0; x<cols; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}
+
+function create2DArray(cols, rows) {
+  let emptyArray = [];
+  for (let y=0; y<rows; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<cols;x++) {
+      emptyArray[y].push(0);
+    }
+  }
+  return emptyArray;
+}
+
+function createRandom2DArray(cols, rows) {
+  let emptyArray = [];
+  for (let y=0; y<rows; y++) {
+    emptyArray.push([]);
+    for (let x=0; x<cols; x++) {
+      if (random(100) < 50) {
+        emptyArray[y].push(0);
+      }
+      else {
+        emptyArray[y].push(1);
+      }
+    }
+  }
+  return emptyArray;
+}
