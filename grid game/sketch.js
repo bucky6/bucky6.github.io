dcc52c44b5b75bd4908c9dcf67b9c68de4a3d979
bucky6 <114016486+bucky6@users.noreply.github.com>
2022-11-02 12:17:05 -0600

    messed around

--- grid game/sketch.js
+++ grid game/sketch.js
@@ -1,12 +1,15 @@
 // Mastermind
 // Bucky Pederson-Bradbury
 // October 28, 2022
+//
+// 
 
 let rows = 12;
 let cols = 7;
 let grid;
 let cellWidth;
 let cellHeight;
+let state = "grey";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,6 +23,8 @@ function draw() {
   displayGrid(grid);
 }
 
+
+// display the grid and set the order of colours
 function displayGrid(grid) {
   for (let y=0; y<rows; y++) {
     for (let x=0; x<cols; x++) {
@@ -52,6 +57,7 @@ function displayGrid(grid) {
   }
 }
 
+// make the grid
 function create2DArray(cols, rows) {
   let emptyArray = [];
   for (let y=0; y<rows; y++) {
@@ -63,6 +69,7 @@ function create2DArray(cols, rows) {
   return emptyArray;
 }
 
+// set up the colour switching when mouse is pressed
 function mousePressed() {
   console.log(mouseX, mouseY);
   let cellWidth = width/grid[0].length;
@@ -73,26 +80,50 @@ function mousePressed() {
 
   if (grid [y][x] === 0) {
     grid[y][x] = 1;
+    state = "yellow";
   }
   else if (grid[y][x] === 1) {
     grid[y][x] = 2;
+    state = "green";
   }
   else if (grid[y][x] === 2) {
     grid[y][x] = 3;
+    state = "blue";
   }
   else if (grid[y][x] === 3) {
     grid[y][x] = 4;
+    state = "red";
   }
   else if (grid[y][x] === 4) {
     grid[y][x] = 5;
+    state = "purple";
   }
   else if (grid[y][x] === 5) {
     grid[y][x] = 6;
+    state = "black";
   }
   else if (grid[y][x] === 6) {
     grid[y][x] = 7;
+    state = "white";
   }
   else if (grid[y][x] === 7) {
     grid[y][x] = 1;
+    state = "yellow";
   }
 }
+
+// store player's choice in an array
+let choiceArray = [];
+function addChoice() {
+  if (state === "yellow") {
+    choiceArray.push("yellow");
+  }
+}
+
+// to do:
+// add button for player to enter choice
+// store colours that the player picked in an array
+// create an ai to choose the colours 
+// work out "right place right colour", "right colour wrong place" 
+// limit colour picking to the row corresponding to the turn
+// 
